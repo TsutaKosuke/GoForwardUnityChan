1c0d5d8416b43b5981a9e12f4ce8bd8ea7c8daf7
TsutaKosuke <kosuke.20051121@icloud.com>
2021-08-02 12:35:13 +0900

    Initial commit

---
+++ Assets/Script/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    //キューブの移動速度
+    private float speed = -12;
+    //消滅位置
+    private float deadLine = -10;
+
+    //課題
+    public AudioClip sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        //画面外に出たら破棄する
+        if (this.transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //課題
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Ground" || tag == "Block")
+        {
+            AudioSource.PlayClipAtPoint(sound, transform.position);
+        }
+    }
+}
